3a5a86cf07b157c80c298441dd519bdc840de9d0
Yann Pfau-Kempf <yann.pfau-kempf@csc.fi>
2025-07-10 19:05:52 +0300

    Added parallel for options passing indices or coordinates to the loop body. Avoids having to call fsgird.* functions from the lambda, whcih breaks in some cases.

--- src/grid.hpp
+++ src/grid.hpp
@@ -497,6 +497,89 @@ public:
       }
    }
 
+   /*! Parallelised for loop interface passing ijk to the loop body*/
+   template <typename Lambda, typename TimerCallBack, typename T>
+   void parallel_for_ijk(TimerCallBack timerCallBack, int timerId, std::span<T> technical, Lambda loop_body) {
+      // Using raw pointer for localSize;
+      // Workaround intel compiler bug in collapsed openmp loops
+      // see https://github.com/fmihpc/vlasiator/commit/604c81142729c5025a0073cd5dc64a24882f1675
+      const FsIndex_t* localSize = &coordinates.localSize[0];
+
+#pragma omp parallel
+      {
+         auto timer = timerCallBack(timerId);
+#pragma omp for collapse(2)
+         for (auto k = 0; k < localSize[2]; k++) {
+            for (auto j = 0; j < localSize[1]; j++) {
+               for (auto i = 0; i < localSize[0]; i++) {
+                  const auto s = makeStencil(i, j, k);
+                  const auto& tech = technical[s.ooo()];
+                  const auto sysBoundaryFlag = tech.sysBoundaryFlag;
+                  const auto sysBoundaryLayer = tech.sysBoundaryLayer;
+                  loop_body(s, sysBoundaryFlag, sysBoundaryLayer, i, j, k);
+               }
+            }
+         }
+         timer.stop(getNumCells(), "Spatial Cells");
+      }
+   }
+
+   /*! Parallelised for loop interface passing global ijk to the loop body*/
+   template <typename Lambda, typename TimerCallBack, typename T>
+   void parallel_for_global_ijk(TimerCallBack timerCallBack, int timerId, std::span<T> technical, Lambda loop_body) {
+      // Using raw pointer for localSize;
+      // Workaround intel compiler bug in collapsed openmp loops
+      // see https://github.com/fmihpc/vlasiator/commit/604c81142729c5025a0073cd5dc64a24882f1675
+      const FsIndex_t* localSize = &coordinates.localSize[0];
+
+#pragma omp parallel
+      {
+         auto timer = timerCallBack(timerId);
+#pragma omp for collapse(2)
+         for (auto k = 0; k < localSize[2]; k++) {
+            for (auto j = 0; j < localSize[1]; j++) {
+               for (auto i = 0; i < localSize[0]; i++) {
+                  const auto s = makeStencil(i, j, k);
+                  const auto& tech = technical[s.ooo()];
+                  const auto sysBoundaryFlag = tech.sysBoundaryFlag;
+                  const auto sysBoundaryLayer = tech.sysBoundaryLayer;
+                  const auto gijk = localToGlobal(i,j,k);
+                  loop_body(s, sysBoundaryFlag, sysBoundaryLayer, gijk[0], gijk[1], gijk[2]);
+               }
+            }
+         }
+         timer.stop(getNumCells(), "Spatial Cells");
+      }
+   }
+
+   /*! Parallelised for loop interface passing physical coordinates to the loop body*/
+   template <typename Lambda, typename TimerCallBack, typename T>
+   void parallel_for_coords(TimerCallBack timerCallBack, int timerId, std::span<T> technical, Lambda loop_body) {
+      // Using raw pointer for localSize;
+      // Workaround intel compiler bug in collapsed openmp loops
+      // see https://github.com/fmihpc/vlasiator/commit/604c81142729c5025a0073cd5dc64a24882f1675
+      const FsIndex_t* localSize = &coordinates.localSize[0];
+
+#pragma omp parallel
+      {
+         auto timer = timerCallBack(timerId);
+#pragma omp for collapse(2)
+         for (auto k = 0; k < localSize[2]; k++) {
+            for (auto j = 0; j < localSize[1]; j++) {
+               for (auto i = 0; i < localSize[0]; i++) {
+                  const auto s = makeStencil(i, j, k);
+                  const auto& tech = technical[s.ooo()];
+                  const auto sysBoundaryFlag = tech.sysBoundaryFlag;
+                  const auto sysBoundaryLayer = tech.sysBoundaryLayer;
+                  const auto coords = getPhysicalCoords(i,j,k);
+                  loop_body(s, sysBoundaryFlag, sysBoundaryLayer, coords);
+               }
+            }
+         }
+         timer.stop(getNumCells(), "Spatial Cells");
+      }
+   }
+
    /*! Same as above parallel_for but without parallelization, for debugging purposes */
    template <typename Lambda, typename TimerCallBack, typename T>
    void serial_for(TimerCallBack timerCallBack, int timerId, std::span<T> technical, Lambda loop_body) {
